3ed5570ca43522185b5c9960154ad158dfd6727c
Shreyy <shriyanshyadav2006@gmail.com>
2025-07-30 21:35:05 +0530

    Create even_values.cpp

---
+++ even_values.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout<<"Even Numbers between 1 to 10 are :\n";
+  for(int i = 1; i <= 10; i++){
+  if (i % 2 == 0) {
+     cout << i << " ";
+    }
+  }
+}
